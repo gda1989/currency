16984a9ac77ec1513fe80ca95b93fe726c999237
gorbachev.d <gorbachev.d@prodv.net>
2022-03-21 17:41:29 +0700

    + немножко дизайна

--- sources/ui/main/src/main/java/com/example/main/MainActivity.kt
+++ sources/ui/main/src/main/java/com/example/main/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.core.DataProvidersFactory
 import com.example.core.dto.Currency
 import com.example.core_api.mediators.AppWithFacade
--- sources/ui/pages/src/main/java/com/example/pages/ui/currency_list/ui/adapter/CurrencyViewHolder.kt
+++ sources/ui/pages/src/main/java/com/example/pages/ui/currency_list/ui/adapter/CurrencyViewHolder.kt
@@ -1,8 +1,10 @@
 package com.example.pages.ui.currency_list.ui.adapter
 
 import android.graphics.Color
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.core_api.entities.ECurrency
+import com.example.pages.ui.currency_list.R
 import com.example.pages.ui.currency_list.databinding.ItemCurrencyRatioBinding
 import java.math.RoundingMode
 
@@ -16,10 +18,10 @@ class CurrencyViewHolder(
         binding.currencyRate.text = ((data.getRate() / baseRate).toBigDecimal()
             .setScale(6, RoundingMode.UP).toDouble()).toString()
         if (data.isChecked()) {
-            binding.isFav.setBackgroundColor(Color.BLUE)
+            binding.isFav.setImageDrawable(ContextCompat.getDrawable(binding.isFav.context, R.drawable.ic_fav_bright))
             binding.isFav.setOnClickListener { clickListener.onDislikeClick(data) }
         } else {
-            binding.isFav.setBackgroundColor(Color.WHITE)
+            binding.isFav.setImageDrawable(ContextCompat.getDrawable(binding.isFav.context, R.drawable.ic_fav_grey))
             binding.isFav.setOnClickListener { clickListener.onLikeClick(data) }
         }
     }
